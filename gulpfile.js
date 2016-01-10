eb8eff7e002481ce8496cee41b92a9dd75ef0927
spencercarnage <spencercarnage@gmail.com>
2016-01-10 14:58:17 -0800

    added browsersync gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@
 var gulp = require('gulp');
 // Load all gulp- node modules
 var $ = require('gulp-load-plugins')();
+var pkg = require('./package.json');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// Load tasks
 var watchifyJSApps = require('./tasks/watchify');
 var watchifyJSWorkers = require('./tasks/watchify-workers');
 var minifyJSApps = require('./tasks/minify');
-var pkg = require('./package.json');
+var browserSyncTask = require('./tasks/browserSync');
 
 gulp.task('watchify-js-apps', watchifyJSApps);
 gulp.task('watchify-js-workers', watchifyJSWorkers);
 gulp.task('minify-js-apps', minifyJSApps);
+gulp.task('browser-sync', browserSyncTask);
 
 // watch for changes to build apps using gulp-watch so we can pick up changes
 // for any new files.
@@ -24,7 +30,7 @@ gulp.task('watch-js-apps', function (done) {
 gulp.task('default', [
     'server',
     'watchify-js-apps',
-    'watchify-js-workers',
+    //'watchify-js-workers',
     'watch-js-apps'
 ]);
 
