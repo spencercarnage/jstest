542ab886368b1985f1ae60757ba3c2883d5369f2
spencercarnage <spencercarnage@gmail.com>
2016-01-25 19:32:38 -0800

    switched to from hapi to express

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 'use strict';
 
-const Hapi = require('hapi');
+const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 3000;
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-// Create a server with a host and port
-const server = new Hapi.Server();
 
-server.connection({
-  host: 'localhost',
-  port: port,
-  labels: ['app']
+// serve static assets
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', './templates');
+app.set('view engine', 'jade');
+
+// Handle all routes so they go to index.html
+app.get('*', function (request, response) {
+  response.render('index');
 });
 
-server.connection({
-  port: 4040,
-  labels: ['quiz']
-});
-
-const io = require('socket.io')(server.select('quiz').listener);
+server.listen(4040);
+app.listen(port);
 
 io.on('connection', function (socket) {
   socket.emit('oh hi!');
@@ -31,53 +34,55 @@ io.on('connection', function (socket) {
   });
 });
 
-const rootHandler = function (request, reply) {
-    reply.view('index', {
-        title: 'example',
-        message: 'hello world'
-    });
-};
+console.log('server started on port ' +  port);
 
-server.register(require('vision'), (err) => {
-    if (err) {
-        throw err;
-    }
-
-    server.views({
-        engines: {
-            jade: require('jade')
-        },
-        path: __dirname + '/templates',
-        compileOptions: {
-            pretty: true
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: rootHandler
-    });
-});
-
-server.register(require('inert'), (err) => {
-    server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: 'public'
-            }
-        }
-    });
-});
-
-
-// Start the server
-server.start((err) => {
-    if (err) {
-        throw err;
-    }
-
-    console.log('Server running at:', server.connections[0].info.uri);
-});
+//const rootHandler = function (request, reply) {
+//    reply.view('index', {
+//        title: 'example',
+//        message: 'hello world'
+//    });
+//};
+//
+//server.register(require('vision'), (err) => {
+//    if (err) {
+//        throw err;
+//    }
+//
+//    server.views({
+//        engines: {
+//            jade: require('jade')
+//        },
+//        path: __dirname + '/templates',
+//        compileOptions: {
+//            pretty: true
+//        }
+//    });
+//
+//    server.route({
+//        method: 'GET',
+//        path: '/*',
+//        handler: rootHandler
+//    });
+//});
+//
+//server.register(require('inert'), (err) => {
+//    server.route({
+//        method: 'GET',
+//        path: '/{param*}',
+//        handler: {
+//            directory: {
+//                path: 'public'
+//            }
+//        }
+//    });
+//});
+//
+//
+//// Start the server
+//server.start((err) => {
+//    if (err) {
+//        throw err;
+//    }
+//
+//    console.log('Server running at:', server.connections[0].info.uri);
+//});
